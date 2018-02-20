ff5ec2285542683ed3d0f2b305b0071b0e8b4e36
Kjell Denti <kjelldg@gmail.com>
2018-02-20 21:00:57 +0100

    La till felhantering igen, ny jarfil och skrev min del av rapporten.

--- src/com/lernia/basiccalc/Main.java
+++ src/com/lernia/basiccalc/Main.java
@@ -1,5 +1,6 @@
-package com.lernia.basiccalc;
+//package com.lernia.basiccalc;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,31 +11,51 @@ public class Main {
 	public static void main(String[] args) {
 		
 		sc = new Scanner(System.in);
-		int first;
-		String str;
-		int second;
+		int first = 0;
+		String str = "";
+		int second = 0;			
 		
-		while (true) {
-			try {
+		while (true) {			
 
-				System.out.println("Type your calculation (x <arithmetic> y) : ");
-				if (sc.hasNext()) {
-					first = sc.nextInt();
-					str = sc.next();
-					second = sc.nextInt();
+				System.out.println("Type your calculation (x <arithmetic> y) , eg 3 + 3 or 111 * 3 : ");
+									
+					try {
+						first = sc.nextInt();												
+					}
 
-					calc(first, setArithmetic(str), second);
-				} else {
-					break;
-				}
+					catch (InputMismatchException e) {
+					    System.out.print("Error. Please insert according to 'x <arithmetic> y', eg 3 + 3 .");
+					    sc.nextLine();
+					}			
+					
+					try {
+						str = sc.next();
+					}
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+					catch (InputMismatchException e) {
+					    System.out.print("Error. Please only insert '+', '-', '+' or '*'.");
+					    //sc.nextLine();
+					}
+					
+					
+					try {
+						second = sc.nextInt();						
+					}
 
+					catch (InputMismatchException e) {
+					    System.out.print("Error. Please insert according to 'x <arithmetic> y', eg 3 + 3 .");
+					    sc.nextLine();
+					}
+										
+					if (str.equals("+") || str.equals("/") || str.equals("-") || str.equals("*")) {
+						calc(first, setArithmetic(str), second);							
+					} else {
+						System.out.println("Error. Please only insert '+', '-', '+' or '*'.");							
+					}			
 		}
 		
 	}
+	
 	
 	private static void calc(int first, calculate rs, int second) {
 		
